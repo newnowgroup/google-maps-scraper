99555d9940e7a32a674e851741e1d516a58ef2ef
marconow <marco.magno@newnow.group>
2024-01-29 13:44:25 +0100

    Update main.py
    scrape over all german cities

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ path_root = os.getenv("ONEDRIVE_PATH")
 if not os.path.isdir(path_root):
     raise "you do not have the ONEDRIVE_PATH environment variable set up. Please set it up first."
 
-queries = ["WMF"]
+queries = Gmaps.Cities.Germany("WMF")
 
 Gmaps.places(queries, scrape_reviews=True, reviews_max=Gmaps.ALL_REVIEWS,
-             convert_to_english=False, path=path_root)
+             convert_to_english=False, path=os.path.join(path_root, 'gmaps_scraping'))
